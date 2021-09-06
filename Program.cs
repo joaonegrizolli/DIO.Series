c3b1355f4d04819ab9f2ec98caf545d3ea9b8af0
joaonegrizolli <jnegrizolli@gmail.com>
2021-09-06 10:48:29 -0300

    programa de cadastro de series bootcamp DIO

---
+++ Program.cs
@@ -0,0 +1,182 @@
+using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                        ListarSeries();
+                        break;
+                    case "2":
+                        InserirSerie();
+                        break;
+                    case "3":
+                        AtualizarSerie();
+                        break;
+                    case "4":
+                        ExcluirSerie();
+                        break;
+                    case "5":
+                        VisualizarSerie();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigdo por utilizar nossos serviços.");
+            Console.ReadLine();
+        }
+
+        private static void ExcluirSerie()
+        {
+            Console.Write("Digide o ID da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(indiceSerie);
+        }
+
+        private static void VisualizarSerie()
+        {
+            Console.Write("Digide o ID da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(indiceSerie);
+
+            Console.WriteLine(serie);
+        }
+        
+        private static void AtualizarSerie()
+        {
+            Console.Write("Digite o ID da série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=netcore-3.1
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=netcore-3.1
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o título da série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o ano de início da série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número de temporadas da série: ");
+            int entradaTemporadas = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a descrição da série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Console.Write("Em que serviço de streaming a série está disponível: ");
+            string entradaOndeAssistir = Console.ReadLine();
+
+            Serie atualizaSerie = new Serie(id: indiceSerie,
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        temporadas: entradaTemporadas,
+                                        ondeAssistir: entradaOndeAssistir,
+                                        descricao: entradaDescricao);
+            
+            repositorio.Atualiza(indiceSerie, atualizaSerie);
+        }
+        private static void ListarSeries()
+        {
+            Console.WriteLine("Listar Séries");
+
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenhuma série encontrada");
+                return;
+            }
+
+            foreach (var serie in lista)
+            {
+                var excluido = serie.retornaExcluido();
+                
+                Console.WriteLine("#ID {0}: - {1} {2}", serie.retornaId(), serie.retornaTitulo(), (excluido ? "*Excluído*" : ""));
+            }
+        }
+
+        private static void InserirSerie()
+        {
+            Console.WriteLine("Inserir nova série");
+
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=netcore-3.1
+            // https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=netcore-3.1
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.Write("Digite o gênero entre as opções acima: ");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o título da série: ");
+            string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o ano de início da série: ");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número de temporadas da série: ");
+            int entradaTemporadas = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a descrição da série: ");
+            string entradaDescricao = Console.ReadLine();
+
+            Console.Write("Em que serviço de streaming a série está disponível: ");
+            string entradaOndeAssistir = Console.ReadLine();
+
+            Serie novaSerie = new Serie(id: repositorio.ProximoId(),
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        temporadas: entradaTemporadas,
+                                        ondeAssistir: entradaOndeAssistir,
+                                        descricao: entradaDescricao);
+            
+            repositorio.Insere(novaSerie);
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("REPOSITÓRIO DE SÉRIES");
+            Console.WriteLine("Informe a opção desejada: ");
+
+            Console.WriteLine("1 - Listar Séries Cadastradas");
+            Console.WriteLine("2 - Inserir uma nova série");
+            Console.WriteLine("3 - Atualizar série");
+            Console.WriteLine("4 - Excluir série");
+            Console.WriteLine("5 - Visualizar série");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+            
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# REPOSITÓRIO DE SÉRIES
+
+Programa de cadastro de séries de televisão desenvolvido no Bootcamp **Decola Tech** da *Digital Innovation One*, com orientação do professor Eliezer Zarpelão.
+
+Desenvolvido em C#, como forma de aplicar os conhecimentos do curso de introdução a .NET.
